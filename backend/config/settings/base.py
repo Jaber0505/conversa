534319de58b8e134d4549df275d95fc85cc2f658
Jaber Bo <j.boudouh.21500@stu.brucity.education>
2025-08-04 08:14:47 +0200

    fix(config) : charge conditionnelle des variables d'environnement uniquement en mode développement

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-if os.getenv("ENV_MODE", "dev") == "dev":
+if os.getenv("ENV_MODE", "dev") != "production":
     from dotenv import load_dotenv
     load_dotenv()
 
