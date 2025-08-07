823edf6bd76f60130fa085f84eea9f94dd7d2fc2
Jaber Bo <j.boudouh.21500@stu.brucity.education>
2025-08-07 13:52:09 +0200

    Correction Test

--- backend/users/tests/views/test_rest_password_views.py
+++ backend/users/tests/views/test_rest_password_views.py
@@ -4,6 +4,7 @@ from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.urls import reverse
+from django.test.utils import override_settings
 from rest_framework import status
 
 
