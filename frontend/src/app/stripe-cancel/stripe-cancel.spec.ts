7ddeca4550d828b4f3c635af713df2a8324eafab
Jaber Bo <j.boudouh.21500@stu.brucity.education>
2025-08-22 13:05:53 +0200

    big update frontend

---
+++ frontend/src/app/stripe-cancel/stripe-cancel.spec.ts
@@ -0,0 +1,23 @@
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+//
+// import { StripeCancel } from './stripe-cancel';
+//
+// describe('StripeCancel', () => {
+//   let component: StripeCancel;
+//   let fixture: ComponentFixture<StripeCancel>;
+//
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [StripeCancel]
+//     })
+//     .compileComponents();
+//
+//     fixture = TestBed.createComponent(StripeCancel);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
